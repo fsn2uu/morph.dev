38c4714de76224f8505321270f116d7306aef911
Kevin Pritchett <fsn2uu@gmail.com>
2022-10-14 07:37:30 -0500

    oops

--- resources/views/admin/reservations/create.blade.php
+++ resources/views/admin/reservations/create.blade.php
@@ -12,21 +12,6 @@
         <form action="{{ route('admin.reservations.store') }}" method="post">
             @csrf
 
-            @php
-                $fields = [
-                    'company_id' => ['type' => 'text', 'required' => true],
-                    'unit_id' => ['type' => 'text', 'required' => true],
-                    'traveler_id' => ['type' => 'text', 'required' => true],
-                    'status' => ['type' => 'text', 'required' => true],
-                    'type' => ['type' => 'text', 'required' => true],
-                    'amount_charged' => ['type' => 'text', 'required' => true],
-                    'stripe_charge_id' => ['type' => 'text', 'required' => true],
-                    'stripe_fees' => ['type' => 'text', 'required' => true],
-                    'start_date' => ['type' => 'text', 'required' => true],
-                    'end_date' => ['type' => 'text', 'required' => true],
-                ];
-            @endphp
-
             <label for="unit_id"  class="block">Unit</label>
             <select name="unit_id" id="unit_id" class="shadow appearance-none border border-[#ccc] mb-2 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline">
                 <option value="">Choose a Unit</option>
